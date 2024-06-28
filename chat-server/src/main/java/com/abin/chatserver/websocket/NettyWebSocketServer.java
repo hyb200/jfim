c74d8685313a15b37f7121ac2bae778c6273c769
hyb200 <13672800746@163.com>
2024-06-28 11:02:34 +0800

    feat: 使用 Netty 实现 WebSocket

---
+++ chat-server/src/main/java/com/abin/chatserver/websocket/NettyWebSocketServer.java
@@ -0,0 +1,66 @@
+package com.abin.chatserver.websocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+import io.netty.util.NettyRuntime;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class NettyWebSocketServer {
+    public static final int WEB_SOCKET_PORT = 10086;
+
+    private EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+    private EventLoopGroup workerGroup = new NioEventLoopGroup(NettyRuntime.availableProcessors());
+
+    /**
+     * 优雅停机
+     */
+    @PreDestroy
+    public void destory() {
+        bossGroup.shutdownGracefully().syncUninterruptibly();
+        workerGroup.shutdownGracefully().syncUninterruptibly();
+        log.info("WebSocket Server is closed");
+    }
+
+    /**
+     * 启动 WebSocket
+     */
+    @PostConstruct
+    public void start() throws InterruptedException {
+        ServerBootstrap server = new ServerBootstrap();
+        server.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 128)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) {
+                        ChannelPipeline pipeline = socketChannel.pipeline();
+                        pipeline.addLast(new HttpServerCodec());
+                        pipeline.addLast(new ChunkedWriteHandler());
+                        pipeline.addLast(new HttpObjectAggregator(8 * 1024));
+                        pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+                        pipeline.addLast(new NettyWebSocketServerHandler());
+                    }
+                });
+        server.bind(WEB_SOCKET_PORT).sync();
+        log.info("WebSocket Server is running on {}", WEB_SOCKET_PORT);
+    }
+}
---
+++ chat-server/src/main/java/com/abin/chatserver/websocket/NettyWebSocketServerHandler.java
@@ -0,0 +1,22 @@
+package com.abin.chatserver.websocket;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.util.concurrent.EventExecutorGroup;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ChannelHandler.Sharable
+public class NettyWebSocketServerHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame msg) {
+        String message = msg.text();
+        System.out.println(message);
+        Channel channel = channelHandlerContext.channel();
+        channel.writeAndFlush(new TextWebSocketFrame("this is websocket, i got your message"));
+    }
+}
