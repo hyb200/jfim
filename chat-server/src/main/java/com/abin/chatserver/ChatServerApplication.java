b4c1ede1f3363fc709d078ac136e01e8c851b417
hyb200 <13672800746@163.com>
2024-07-28 23:12:23 +0800

    fix: 日志时间修改

--- chat-server/src/main/java/com/abin/chatserver/ChatServerApplication.java
+++ chat-server/src/main/java/com/abin/chatserver/ChatServerApplication.java
@@ -1,12 +1,15 @@
 package com.abin.chatserver;
 
 import com.abin.chatserver.common.config.EnableRedisSerialize;
+import jakarta.annotation.PostConstruct;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import java.util.TimeZone;
 
 
 @SpringBootApplication(scanBasePackages = "com.abin")
@@ -19,4 +22,8 @@ public class ChatServerApplication {
         SpringApplication.run(ChatServerApplication.class, args);
     }
 
+    @PostConstruct
+    public void setDefaultTimezone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+    }
 }
